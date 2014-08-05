d5a38813e34a5bd24a306032770891ccd15cb275
Brandon Richey <diamondgfx@gmail.com>
2014-08-04 23:40:45 -0400

    Game has a working UI, a better title screen, and 20 levels. Not too shabby.

--- Source/Explosion.m
+++ Source/Explosion.m
@@ -20,7 +20,7 @@
     self.active = TRUE;
     self.position              = location;
     self.anchorPoint           = ccp(0.5, 0.5);
-    self.scale                 = 0.2f;
+    self.scale                 = 0.1f;
     CCActionScaleBy  *expand   = [CCActionScaleBy actionWithDuration:1.7f scale:10.0f];
     CCActionDelay    *delay    = [CCActionDelay actionWithDuration:0.7f];
     CCActionScaleBy  *shrink   = [CCActionScaleBy actionWithDuration:0.7f scale:0.0f];
--- Source/MainScene.m
+++ Source/MainScene.m
@@ -18,12 +18,15 @@
     BOOL           _ranOnce;
     int            _score;
     CCLabelTTF     *_scoreDisplay;
+    int            _level;
+    int            _bubbleMax;
+    int            _loadBubbles;
+    int            _totalScore;
 }
 
 static const CGFloat  MIN_ANGLE        = 0.0f;
 static const CGFloat  MAX_ANGLE        = 360.0f * M_PI / 180.0f;
 static const CGFloat  BUBBLE_SPEED     = 30.0f;
-static const int      STARTING_BUBBLES = 30;
 
 static inline CGVector radiansToVector(CGFloat radians)
 {
@@ -47,17 +50,105 @@ static inline CGFloat randomInRange(CGFloat low, CGFloat high)
     if (self = [super init]) {
         _bubbles = [NSMutableArray array];
         self.userInteractionEnabled = TRUE;
-        _ranOnce = FALSE;
-        _score   = 0;
+        _ranOnce    = FALSE;
+        _score      = 0;
+        _level      = 0;
+        _totalScore = 0;
     }
     return self;
 }
 
+-(void)resetStage
+{
+    for (Bubble *bubble in _bubbles) {
+        [bubble removeFromParentAndCleanup:TRUE];
+    }
+    [_bubbles removeAllObjects];
+    _ranOnce = FALSE;
+    _score = 0;
+}
+
+-(void)loadStage
+{
+    switch (_level) {
+        case 19:
+            [self buildStage:40 andNextLevelAt:38];
+            break;
+        case 18:
+            [self buildStage:40 andNextLevelAt:37];
+            break;
+        case 17:
+            [self buildStage:40 andNextLevelAt:35];
+            break;
+        case 16:
+            [self buildStage:35 andNextLevelAt:32];
+            break;
+        case 15:
+            [self buildStage:35 andNextLevelAt:31];
+            break;
+        case 14:
+            [self buildStage:35 andNextLevelAt:30];
+            break;
+        case 13:
+            [self buildStage:35 andNextLevelAt:28];
+            break;
+        case 12:
+            [self buildStage:30 andNextLevelAt:26];
+            break;
+        case 11:
+            [self buildStage:30 andNextLevelAt:24];
+            break;
+        case 10:
+            [self buildStage:25 andNextLevelAt:22];
+            break;
+        case 9:
+            [self buildStage:25 andNextLevelAt:19];
+            break;
+        case 8:
+            [self buildStage:25 andNextLevelAt:17];
+            break;
+        case 7:
+            [self buildStage:20 andNextLevelAt:15];
+            break;
+        case 6:
+            [self buildStage:20 andNextLevelAt:13];
+            break;
+        case 5:
+            [self buildStage:15 andNextLevelAt:10];
+            break;
+        case 4:
+            [self buildStage:12 andNextLevelAt:7];
+            break;
+        case 3:
+            [self buildStage:10 andNextLevelAt:5];
+            break;
+        case 2:
+            [self buildStage:7 andNextLevelAt:3];
+            break;
+        case 1:
+            [self buildStage:5 andNextLevelAt:2];
+            break;
+        case 0:
+        default:
+            [self buildStage:5 andNextLevelAt:1];
+            break;
+    }
+}
+
+-(void)buildStage:(int)bubbleCount andNextLevelAt:(int)nextLevel
+{
+    [self resetStage];
+    _loadBubbles = bubbleCount;
+    _bubbleMax   = nextLevel;
+    [self updateDisplay];
+    [self setupBubbles];
+}
+
 -(void)setupBubbles
 {
-    for (int i = 0; i < STARTING_BUBBLES; i++) {
+    for (int i = 0; i < _loadBubbles; i++) {
         Bubble *bubble = (Bubble*)[CCBReader load:@"BetterBubble"];
-        bubble.scale = 0.25f;
+        bubble.scale = 0.1f;
         CGVector direction = radiansToVector(randomInRange(MIN_ANGLE, MAX_ANGLE));
         bubble.velocity = ccp(BUBBLE_SPEED * direction.dx, BUBBLE_SPEED * direction.dy);
         bubble.name = [NSString stringWithFormat:@"bubble_%d", i];
@@ -83,8 +174,8 @@ static inline CGFloat randomInRange(CGFloat low, CGFloat high)
 -(void)onEnter
 {
     [super onEnter];
-    [self setupBubbles];
     [self setupExplosion];
+    [self loadStage];
 }
 
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
@@ -123,12 +214,48 @@ static inline CGFloat randomInRange(CGFloat low, CGFloat high)
             }
         }
     }
+    if ([self isCycleDone]) {
+        if (_score >= _bubbleMax) {
+            [self transitionToNextStage];
+        } else {
+            [self redoStage];
+        }
+    }
+}
+
+-(BOOL)isCycleDone
+{
+    if (!_ranOnce || _explosion.active) {
+        return FALSE;
+    }
+    for (Bubble *bubble in _bubbles) {
+        if (bubble.exploding) {
+            return FALSE;
+        }
+    }
+    return TRUE;
 }
 
 -(void)addPoint
 {
     _score++;
-    _scoreDisplay.string = [NSString stringWithFormat:@"Score: %d / %d", _score, STARTING_BUBBLES];
+    [self updateDisplay];
+}
+
+-(void)updateDisplay
+{
+    _scoreDisplay.string = [NSString stringWithFormat:@"Level %d Score: %d / %d", _level+1, _score, _bubbleMax];
+}
+
+-(void)transitionToNextStage
+{
+    _level++;
+    [self loadStage];
+}
+
+-(void)redoStage
+{
+    [self loadStage];
 }
 
 @end
